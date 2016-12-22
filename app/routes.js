979b08d381ef4fdf466a60f3701196a18581799a
coyotte508 <coyotte508@gmail.com>
2016-12-23 00:28:12 +0100

    Use layouts

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", {error:null});
+  res.render("pages/index", {error:null});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const router = require('./app/routes');
 
 var app = express();
 var port = 8080;
 
 app.set('view engine', 'ejs'); 
+
+app.use(expressLayouts);
 
 app.use("/", express.static(__dirname + '/public'));
 app.use("/", router);
