90cc995ea64e9828a54450d6474534ed134f1770
coyotte508 <coyotte508@gmail.com>
2016-12-30 11:26:06 +0100

    Contact page

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.render("pages/index", {error:null, req});
 });
 
+router.get("/contact", (req, res) => {
+  res.render("pages/contact", {error:null, req});
+});
+
 /* Aggregate routers */
 [require("./auth.js"), require("./novel.js"), require("./user.js")].forEach((mod) => router.use("/", mod));
 
