6158d60bfa8d873e22a95003185c9bbdeeae1310
coyotte508 <coyotte508@gmail.com>
2017-01-27 15:52:09 +0100

    Improve admin interface

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,8 +1,33 @@
 const router = require("express").Router();
 const utils = require("./utils");
+const co = require("co");
+const Novel = require("../models/novel");
+const Chapter = require("../models/chapter");
+const User = require("../models/user");
+const limiter = require("mongo-limiter");
 
 router.get("/admin", utils.isAdmin, (req, res) => {
-  res.render("pages/admin", {req});
+  co(function *(){
+    var [nbNovels, nbChapters, nbUsers, actions] = yield Promise.all([Novel.count(),Chapter.count(), User.count(), limiter.logs()]);
+
+    var userIds = actions.map(x => x.user).filter(x => !x.includes(".") && !x.includes(":"));
+    var users = yield User.find({_id: {$in: userIds}});
+    var userD = {};
+    users.forEach(x => userD[x.id] = x);
+
+    actions.forEach(x => {
+      if (x.user in userD) {
+        x.userLink = userD[x.user].getLink();
+        x.user = userD[x.user].displayName();
+      }
+    });
+
+    var stats = {
+      nbUsers, nbChapters, nbNovels
+    };
+
+    res.render("pages/admin", {actions, req, stats});
+  })
 });
 
 module.exports = router;
