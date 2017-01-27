016381387d155c5b367b5563ab5521196442fd0f
coyotte508 <coyotte508@gmail.com>
2017-01-27 16:00:22 +0100

    Another improvement to admin display

--- app/routes/novel.js
+++ app/routes/novel.js
@@ -58,36 +58,6 @@ router.param('novel', function(req, res, next, novel) {
 
     req.novel = novel;
 
-    req.unixTime = function(date) {
-      return parseInt(date.substring(0, 8), 16);
-    }
-
-    req.timeSince = function(date) {
-      var seconds = Math.floor((Date.now()/ 1000 - this.unixTime(date)));
-      var interval = Math.floor(seconds / 31536000);
-
-      if (interval > 1) {
-        return interval + " years";
-      }
-      interval = Math.floor(seconds / 2592000);
-      if (interval > 1) {
-        return interval + " months";
-      }
-      interval = Math.floor(seconds / 86400);
-      if (interval > 1) {
-        return interval + " days";
-      }
-      interval = Math.floor(seconds / 3600);
-      if (interval > 1) {
-        return interval + " hours";
-      }
-      interval = Math.floor(seconds / 60);
-      if (interval > 1) {
-        return interval + " minutes";
-      }
-      return Math.floor(seconds) + " seconds";
-    }
-
     req.categoryName = cat => categories.find(x => x.shorthand == cat).name;
 
     next();
--- server.js
+++ server.js
@@ -64,14 +64,43 @@ app.use(cookieParser());
 app.use(flash());
 
 app.use(function(req, res, next){
-    /* For css cache busting */
-    req.version = pjson.version;
-    req.makeDescription = descr => {
-      descr = descr.replace(/(<([^>]+)>)/g, "") ||"";
-      var index = descr.indexOf(" ", 150);
-      return descr.substr(0, index == -1 ? undefined : index);
+  /* For css cache busting */
+  req.version = pjson.version;
+  req.makeDescription = descr => {
+    descr = descr.replace(/(<([^>]+)>)/g, "") ||"";
+    var index = descr.indexOf(" ", 150);
+    return descr.substr(0, index == -1 ? undefined : index);
+  }
+  req.unixTime = function(date) {
+    return parseInt(date.substring(0, 8), 16);
+  }
+
+  req.timeSince = function(date) {
+    var seconds = Math.floor((Date.now()/ 1000 - this.unixTime(date)));
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+      return interval + " years";
     }
-    next();
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
+  next();
 });
 
 /* Required for passport */
